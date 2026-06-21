8cb2cd7b48d53cd3a9ff497ef402b8c396fad3a9
darkrider-code <55695504+darkrider-code@users.noreply.github.com>
2026-06-21 17:23:44 +0200

    Update TranslatedPostBody to use @mdx-js/react for runtime rendering

--- components/TranslatedPostBody.tsx
+++ components/TranslatedPostBody.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { MDX } from "@next/mdx";
+import { MDXProvider } from "@mdx-js/react";
 import { mdxComponents } from "@/components/mdx-components";
 import type { Post } from "@/types/post";
+import { compile, run } from "@mdx-js/mdx";
+import * as runtime from "react/jsx-runtime";
 
 interface TranslatedPostBodyProps {
   post: Post;
   content: string;
 }
 
-export default function TranslatedPostBody({ post, content }: TranslatedPostBodyProps) {
+export default async function TranslatedPostBody({ post, content }: TranslatedPostBodyProps) {
+  // Compile MDX content at runtime
+  const compiled = await compile(content, {
+    outputFormat: 'function-body',
+    development: false,
+  });
+  
+  // Create component from compiled code
+  const MdxComponent = new Function('props', compiled) as (props: any) => React.ReactNode;
+  
   return (
     <div className="prose prose-invert max-w-none">
-      <MDX source={content} components={mdxComponents} />
+      <MDXProvider components={mdxComponents}>
+        <MdxComponent />
+      </MDXProvider>
     </div>
   );
 }
